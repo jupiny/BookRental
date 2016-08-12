bad0a66f851cc69f463d408a5be3691bf105d14b
jupiny <tmdghks584@gmail.com>
2016-08-12 23:15:36 +0900

    init BookInfo model: destroy BookInfo model

--- bookrental/db/schema.rb
+++ bookrental/db/schema.rb
@@ -11,17 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811165322) do
+ActiveRecord::Schema.define(version: 20160812141315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
+  create_table "book_infos", force: :cascade do |t|
     t.string   "field"
     t.string   "bookname"
     t.string   "author"
     t.string   "publisher"
-    t.integer  "quantity"
     t.string   "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
