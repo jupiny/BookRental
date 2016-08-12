f3294832439a93ece3bcc1657e58de232244d616
jupiny <tmdghks584@gmail.com>
2016-08-12 23:55:22 +0900

    add seeds: put data in BookInfo and Book models from books.xlsx

--- bookrental/db/seeds.rb
+++ bookrental/db/seeds.rb
@@ -10,12 +10,20 @@
 books_xlsx = Roo::Excelx.new("app/assets/data/books.xlsx").sheet(0)
 
 (3..books_xlsx.last_row).each do |i|
-    row = books_xlsx.row(i)
-    Book.create(
-        field: row[0],
-        bookname: row[1],
-        author: row[2],
-        publisher: row[3],
-        quantity: row[4].to_i,
-        isbn: row[5],
+  row = books_xlsx.row(i)
+  bookinfo = BookInfo.create(
+    field: row[0],
+    bookname: row[1],
+    author: row[2],
+    publisher: row[3],
+    isbn: row[5],
+  )
+  quantity = row[4].to_i 
+  (1..quantity).each do |q|
+    bookinfo.books.create(
+      is_damaged: false,
+      is_lost: false,
+      is_loaned: false,
     )
+  end
+end
