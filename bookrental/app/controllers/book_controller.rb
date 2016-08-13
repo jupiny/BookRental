47f4dba3a8db2ccc3e57e72acb221dbd3e8e5148
jupiny <tmdghks584@gmail.com>
2016-08-14 01:08:48 +0900

    update BookController: add show action

--- bookrental/app/controllers/book_controller.rb
+++ bookrental/app/controllers/book_controller.rb
@@ -2,4 +2,8 @@ class BookController < ApplicationController
   def borrowed
     @borrowed_books = current_user.borrowed_books
   end
+
+  def show 
+    @book = Book.find(params[:id])
+  end
 end
--- bookrental/config/routes.rb
+++ bookrental/config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/bookinfos', to: 'book_info#index'
   get '/bookinfos/:id', to: 'book_info#show', as: 'bookinfo'
   get '/books/borrowed', to: 'book#borrowed', as: 'borrowed_books'
+  get '/books/:id', to: 'book#show', as: 'book'
 end
