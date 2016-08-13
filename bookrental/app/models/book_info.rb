246a0a50a827e6b772a7f2468cf714fa0e2d762a
jupiny <tmdghks584@gmail.com>
2016-08-14 00:24:08 +0900

    update BookInfo model: add is_borrowable_str function

--- bookrental/app/models/book_info.rb
+++ bookrental/app/models/book_info.rb
@@ -1,3 +1,12 @@
 class BookInfo < ActiveRecord::Base
   has_many :books, dependent: :destroy
+
+  def is_borrowable_str
+    for book in self.books
+      if not book.borrower
+        return "대출가능"
+      end
+    end
+    return "대출불가"
+  end
 end
