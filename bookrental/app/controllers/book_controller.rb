8f906ecdebc5e95b51e528291741221d2fd531e9
jupiny <tmdghks584@gmail.com>
2016-08-14 00:49:42 +0900

    update BookController: add borrowed action

--- bookrental/app/controllers/book_controller.rb
+++ bookrental/app/controllers/book_controller.rb
@@ -1,2 +1,5 @@
 class BookController < ApplicationController
+  def borrowed
+    @borrowed_books = current_user.borrowed_books
+  end
 end
--- bookrental/config/routes.rb
+++ bookrental/config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   get '/bookinfos', to: 'book_info#index'
   get '/bookinfos/:id', to: 'book_info#show', as: 'bookinfo'
+  get '/books/borrowed', to: 'book#borrowed', as: 'borrowed_books'
 end
