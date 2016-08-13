5652a279dd5b24a2fed11cf89b226628ac39f3e2
jupiny <tmdghks584@gmail.com>
2016-08-14 02:31:44 +0900

    update BookController: add check_out action

--- bookrental/config/routes.rb
+++ bookrental/config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get '/bookinfos/:id', to: 'book_info#show', as: 'bookinfo'
   get '/books/borrowed', to: 'book#borrowed', as: 'borrowed_books'
   get '/books/:id', to: 'book#show', as: 'book'
+  patch '/books/:id', to: 'book#check_out', as: 'borrow_book'
 end
