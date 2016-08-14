b9eeb6c1cf32c263e70fb20515294a13d25e5802
jupiny <tmdghks584@gmail.com>
2016-08-14 21:51:12 +0900

    beautify sign in, sign up views

--- bookrental/config/initializers/assets.rb
+++ bookrental/config/initializers/assets.rb
@@ -11,5 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += [
   'clean-blog.css',
+  'devise.css',
+
   'clean-blog.js',
 ] 
