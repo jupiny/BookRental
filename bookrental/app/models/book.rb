62ee151fe23d709eb133a56b8e8781c503b9c3bc
jupiny <tmdghks584@gmail.com>
2016-08-15 00:45:12 +0900

    update Book model: add is_borrowable, is_damaged_str, is_lost_str functions

--- bookrental/app/models/book.rb
+++ bookrental/app/models/book.rb
@@ -2,10 +2,24 @@ class Book < ActiveRecord::Base
   belongs_to :book_info
   belongs_to :borrower, :class_name => "User", :foreign_key => :user_id
 
-  def is_borrowable_str
-    if self.borrower
-      return "대출불가"
+  def is_borrowable
+    if not self.borrower and not self.is_lost
+      return true 
     end
-    return "대출가능"
+    return false
+  end
+
+  def is_damaged_str
+    if self.is_damaged
+      return "O"
+    end
+    return "X"
+  end
+
+  def is_lost_str
+    if self.is_lost
+      return "O"
+    end
+    return "X"
   end
 end
